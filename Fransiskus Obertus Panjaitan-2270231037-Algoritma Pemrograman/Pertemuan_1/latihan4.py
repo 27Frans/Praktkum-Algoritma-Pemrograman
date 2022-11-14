dd130a42f05c40ddae30f84cb9ec35a1007b0b00
27Frans <118178600+27Frans@users.noreply.github.com>
2022-11-14 18:41:09 +0700

    Add files via upload

---
+++ Fransiskus Obertus Panjaitan-2270231037-Algoritma Pemrograman/Pertemuan_1/latihan4.py
@@ -0,0 +1,34 @@
+#tipe data Boolean
+
+print(True)
+
+#tipe data String
+#print("Ayo belajar Python")
+print('Belajar Python Sangat Mudah')
+
+#tipe data Integer
+print(20)
+
+#tipe data Float
+print(3.14)
+
+#tipe data Hexadecimal
+#print(9a)
+
+#tipe data Complex
+print(5j)
+
+#tipe data list
+print([1,2,3,4,5])
+print(["satu","dua","tiga"])
+
+#tipe data tuple
+print([1,2,3,4,5])
+print(["satu","dua","tiga"])
+
+#tipe data ditionary
+print({"nama":"budi",'umur':20})
+#tipe data dinionary dimasukan ke dalam variable biodata
+biodata = {"nama":"andi",'umur':21}
+print(biodata)
+print(type(biodata))
